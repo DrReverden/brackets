d7eb4d1ea6e6c6191e7705f5f7f64f2c21452355
DrReverden <lamafon@mail.ru>
2023-02-27 21:38:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length < 2) {
+    return false;
+  }
+  let stack = [];
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i]
+    if (isClosedBracket(current)) {
+      if (brackets[current] !== stack.pop()) return false;
+    } else {
+      stack.push(current);
+    }
+  }
+  return true;
+
+  function isClosedBracket(ch) {
+    return ['(', '[', '{', '1', '3', '6', '7', '8', '|'].indexOf(ch) > -1;
+  }
+  
 }
